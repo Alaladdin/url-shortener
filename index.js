2e61ac234181cee2bce328d66ddda18f123912d5
Aladdin <g.tabroid@gmail.com>
2021-02-04 08:04:05 +0300

    Security was improved

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const path = require('path');
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const xss = require('xss-clean');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+// const cors = require('cors');
 const mongoose = require('mongoose');
 const route = require('./app/routes/ShortenerRoute.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const limit = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests',
+});
+
 require('dotenv').config();
 
 app.set('views', path.join(__dirname, './app/views'));
 app.set('view engine', 'ejs');
-app.use(express.static('vendor'));
 
-app.use(express.json());
+app.use(express.static('vendor'));
+app.use(express.json({ limit: '1kb' }));
+app.use(limit);
+app.use(xss());
+app.use(helmet());
+app.use(mongoSanitize());
+// app.use(cors()); // if u want to enable CORS
 app.use('', route);
 
 mongoose.connect(process.env.MONGO_URI, {
