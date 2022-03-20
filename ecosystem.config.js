fc722bec577c7cae6e01f16d1ce7f8c14b281014
Aladdin <g.tabroid@gmail.com>
2022-03-21 00:11:09 +0300

    v 2.0.3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,27 +3,22 @@ require('dotenv').config();
 module.exports = {
   apps: [
     {
-      name        : 'url-short',
-      script      : 'index.js',
-      watch       : true,
-      ignore_watch: ['node_modules'],
-      env         : {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
+      name          : 'shortener',
+      exec_mode     : 'cluster',
+      script        : 'index.js',
+      watch         : true,
+      wait_ready    : true,
+      ignore_watch  : ['node_modules'],
+      env           : { NODE_ENV: 'development' },
+      env_production: { NODE_ENV: 'production' },
     },
   ],
   deploy: {
     // "production" is the environment name
     production: {
-      key : process.env.DEPLOY_PUBLIC_KEY,
-      user: process.env.DEPLOY_USER,
-      host: [{
-        host: process.env.DEPLOY_HOST,
-        port: process.env.DEPLOY_PORT,
-      }],
+      key          : process.env.DEPLOY_PUBLIC_KEY,
+      user         : process.env.DEPLOY_USER,
+      host         : [{ host: process.env.DEPLOY_HOST, port: process.env.DEPLOY_PORT }],
       ref          : 'origin/main',
       repo         : 'https://github.com/Alaladdin/url-shortener',
       path         : '$HOME/dev/url-shortener',
